7251ffc27c7199def1592ce6b49780412926ba29
19971112 <warashinatomorrow1112@gmail.com>
2019-11-01 18:33:49 +0900

    Update phylogeny_roary_2019-11-01.sh

--- scripts/phylogeny_roary_2019-11-01.sh
+++ scripts/phylogeny_roary_2019-11-01.sh
@@ -11,11 +11,14 @@ mkdir GsppRepCom+out && cd $_
 # Downloading assembly_summary_refseq.txt files
 mkdir -p ./data
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/assembly_summary_refseq.txt
-organism_name="Geobacter"
-outgroup="Pelobacter propionicus"
+
 TYPE="_genomic.gbff.gz"
-cat assembly_summary_refseq.txt | awk -F "\t" '$5 ~ /reference|representative/ && $8 ~ /'"$organism_name"'/ && $11=="latest" && $12 ~ /Complete Genome/ {print $0}' | cut -f20 > filepath_$organism_name
-cat assembly_summary_refseq.txt | awk -F "\t" '$8 ~ /'"$outgroup"'/ && $11=="latest" {print $0}' | cut -f20 >> filepath_$organism_name
+organism_name='Desulfuromonas|Desulfuromusa|Malonomonas|Geopsychrobacter|Geoalkalibacter|Pelobacter|Geobacter' # Desulfuromonadales
+organism_name2='Stigmatella|Corallococcus|Myxococcus|Anaeromyxobacter|Haliangium|Sorangium' # Myxococcales
+
+
+cat assembly_summary_refseq.txt | awk -F "\t" '$8 ~ /'"$organism_name"'/ && $11=="latest" && $12 ~ /Chromosome|Complete Genome/ {print $0}' | cut -f20 > filepath_$organism_name
+cat assembly_summary_refseq.txt | awk -F "\t" '$8 ~ /'"$organism_name2"'/ && $11=="latest" && $12 ~ /Chromosome|Complete Genome/ {print $0}' | cut -f20 >> filepath_$organism_name
 cat filepath_$organism_name | while read FILE; do LINE=$(echo $FILE | sed -n 1p); NAME=$(echo $LINE | cut -d "/" -f 10 ); echo $LINE/$NAME$TYPE >> lists$organism_name.txt; done
 wget -i lists$organism_name.txt
 gunzip *.gz
