182647695990b9c9df6099d2227f4b14e6586361
19971112 <warashinatomorrow1112@gmail.com>
2019-11-01 18:35:59 +0900

    Update phylogeny_roary_2019-11-01.sh

--- scripts/phylogeny_roary_2019-11-01.sh
+++ scripts/phylogeny_roary_2019-11-01.sh
@@ -16,7 +16,6 @@ TYPE="_genomic.gbff.gz"
 organism_name='Desulfuromonas|Desulfuromusa|Malonomonas|Geopsychrobacter|Geoalkalibacter|Pelobacter|Geobacter' # Desulfuromonadales
 organism_name2='Stigmatella|Corallococcus|Myxococcus|Anaeromyxobacter|Haliangium|Sorangium' # Myxococcales
 
-
 cat assembly_summary_refseq.txt | awk -F "\t" '$8 ~ /'"$organism_name"'/ && $11=="latest" && $12 ~ /Chromosome|Complete Genome/ {print $0}' | cut -f20 > filepath_$organism_name
 cat assembly_summary_refseq.txt | awk -F "\t" '$8 ~ /'"$organism_name2"'/ && $11=="latest" && $12 ~ /Chromosome|Complete Genome/ {print $0}' | cut -f20 >> filepath_$organism_name
 cat filepath_$organism_name | while read FILE; do LINE=$(echo $FILE | sed -n 1p); NAME=$(echo $LINE | cut -d "/" -f 10 ); echo $LINE/$NAME$TYPE >> lists$organism_name.txt; done
