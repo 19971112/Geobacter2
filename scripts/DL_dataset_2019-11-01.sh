2e01785d1404097b9147ed8c2a12ae38fd787c2f
19971112 <warashinatomorrow1112@gmail.com>
2019-11-02 13:48:43 +0900

    Update DL_dataset_2019-11-01.sh

--- scripts/DL_dataset_2019-11-01.sh
+++ scripts/DL_dataset_2019-11-01.sh
@@ -10,15 +10,16 @@ mkdir -p ./data && cd $_
 mkdir -p $(date +%F) 
 
 # Downloading assembly_summary_refseq.txt files
-mkdir -p ./data
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/assembly_summary_refseq.txt
 
 TYPE="_genomic.gbff.gz"
 organism_name='Desulfuromonas|Desulfuromusa|Malonomonas|Geopsychrobacter|Geoalkalibacter|Pelobacter|Geobacter' # Desulfuromonadales
 organism_name2='Stigmatella|Corallococcus|Myxococcus|Anaeromyxobacter|Haliangium|Sorangium' # Myxococcales
 
-cat assembly_summary_refseq.txt | awk -F "\t" '$8 ~ /'"$organism_name"'/ && $11=="latest" && $12 ~ /Chromosome|Complete Genome/ {print $0}' | cut -f20 > filepath_$organism_name
-cat assembly_summary_refseq.txt | awk -F "\t" '$8 ~ /'"$organism_name2"'/ && $11=="latest" && $12 ~ /Chromosome|Complete Genome/ {print $0}' | cut -f20 >> filepath_$organism_name
-cat filepath_$organism_name | while read FILE; do LINE=$(echo $FILE | sed -n 1p); NAME=$(echo $LINE | cut -d "/" -f 10 ); echo $LINE/$NAME$TYPE >> lists$organism_name.txt; done
-wget -i lists$organism_name.txt
+cat assembly_summary_refseq.txt | awk -F "\t" '$8 ~ /'"$organism_name"'/ && $11=="latest" && $12 ~ /Chromosome|Complete Genome/ {print $0}' | cut -f20 > DL_list
+cat assembly_summary_refseq.txt | awk -F "\t" '$8 ~ /'"$organism_name2"'/ && $11=="latest" && $12 ~ /Chromosome|Complete Genome/ {print $0}' | cut -f20 >> DL_list
+cat DL_list | while read FILE; do LINE=$(echo $FILE | sed -n 1p); NAME=$(echo $LINE | cut -d "/" -f 10 ); echo $LINE/$NAME$TYPE >> DL_path.txt; done
+wget -i DL_path.txt
 gunzip *.gz
+
+mv *.gz $(date +%F) 
