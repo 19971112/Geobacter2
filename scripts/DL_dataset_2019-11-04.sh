5a8343354aafe386be40094ad8f77e4daad81250
19971112 <warashinatomorrow1112@gmail.com>
2019-11-04 16:26:00 +0900

    Update DL_dataset_2019-11-04.sh

--- scripts/DL_dataset_2019-11-04.sh
+++ scripts/DL_dataset_2019-11-04.sh
@@ -6,7 +6,7 @@
 cd ${PBS_O_WORKDIR}
 
 mkdir -p ./data && cd $_
-mkdir -p dataset2
+mkdir -p dataset
 
 # Downloading assembly_summary_refseq.txt files
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/assembly_summary_refseq.txt
