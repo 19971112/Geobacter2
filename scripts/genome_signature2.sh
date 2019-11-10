4958f3ae27300c65313233bd29b0168760e4acd6
19971112 <warashinatomorrow1112@gmail.com>
2019-11-10 15:33:57 +0900

    Update genome_signature2.sh

--- scripts/genome_signature2.sh
+++ scripts/genome_signature2.sh
@@ -4,6 +4,7 @@ mkdir -p analysis/Genome_signature2/gbk && cd $_
 
 # copy GenBank files and change extension
 cp ../../../data/dataset3/*.gbff .
+for filename in *.gbff; do mv $filename ${filename%.gbff}.gbk; done
 cd ..
 
 for FILE in `ls -Sr gbk/*.gbff`; do echo -n $FILE" "; qsub -v gbk="$FILE" ../../scripts/run_g_genome.sh; done > log.$(date +%F).txt
