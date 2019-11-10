66f911dd0ef84e19fb78c41b68d15dd5872efdd1
19971112 <warashinatomorrow1112@gmail.com>
2019-11-10 16:04:37 +0900

    Update genome_signature2.sh

--- scripts/genome_signature2.sh
+++ scripts/genome_signature2.sh
@@ -8,3 +8,6 @@ for filename in *.gbff; do mv $filename ${filename%.gbff}.gbk; done
 cd ..
 
 for FILE in `ls -Sr gbk/*.gbff`; do echo -n $FILE" "; qsub -v gbk="$FILE" ../../scripts/run_g_genome.sh; done > log.$(date +%F).txt
+
+for i in g_genome/*.txt; do head -n1 $i > g_genome_all.txt; done
+for i in g_genome/*.txt; do tail -n +2 $i >> g_genome_all.txt ;done
