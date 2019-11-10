6521312518c689f5ddbb5af9f24c0b324aff6988
19971112 <warashinatomorrow1112@gmail.com>
2019-11-10 16:07:34 +0900

    Update genome_signature2.sh

--- scripts/genome_signature2.sh
+++ scripts/genome_signature2.sh
@@ -7,7 +7,5 @@ cp ../../../data/dataset3/*.gbff .
 for filename in *.gbff; do mv $filename ${filename%.gbff}.gbk; done
 cd ..
 
-for FILE in `ls -Sr gbk/*.gbff`; do echo -n $FILE" "; qsub -v gbk="$FILE" ../../scripts/run_g_genome.sh; done > log.$(date +%F).txt
+for FILE in `ls -Sr gbk/*.gbk`; do echo -n $FILE" "; qsub -v gbk="$FILE" ../../scripts/run_g_genome.sh; done > log.$(date +%F).txt
 
-for i in g_genome/*.txt; do head -n1 $i > g_genome_all.txt; done
-for i in g_genome/*.txt; do tail -n +2 $i >> g_genome_all.txt ;done
