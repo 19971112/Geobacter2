7b3ee6a18b593c509e6d2a414bdafa849db4c744
19971112 <warashinatomorrow1112@gmail.com>
2019-11-12 15:29:19 +0900

    Update ex_16SrRNA2.py

--- scripts/ex_16SrRNA2.py
+++ scripts/ex_16SrRNA2.py
@@ -7,18 +7,19 @@ FNAME = sys.argv[1]
 gbank=SeqIO.parse(open(FNAME,"rU"),"genbank")
 
 for genome in gbank:
+    i = 0
     parental_seq = genome.seq
     for gene in genome.features:
         if gene.type=="rRNA":
             if 'product' in gene.qualifiers:
                 if '16S' in gene.qualifiers['product'][0]:
-
                     seq_slice = gene.location.extract(parental_seq)
+                    i = i+1
                     if 'db_xref' in gene.qualifiers:
                         gi=[]
                         gi=str(gene.qualifiers['db_xref'])
                         gi=gi.split(":")[1]
                         gi=gi.split("'")[0]
-                        print(">GeneId|%s|16SrRNA|%s\n%s" % (gi,genome.description,seq_slice))
+                        print(">GeneId|%s|16SrRNA_%s|%s\n%s" % (gi,i,genome.description,seq_slice))
                     else:
-                        print(">GeneId|NoGenID|16SrRNA|%s\n%s" % (genome.description,seq_slice))
+                        print(">GeneId|NoGenID|16SrRNA_%s|%s\n%s" % (i,genome.description,seq_slice))
