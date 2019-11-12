02f75be993dee746afd8eb6568d4ed80067e95c8
19971112 <warashinatomorrow1112@gmail.com>
2019-11-12 15:07:39 +0900

    Update RSUC.sh

--- scripts/RSUC.sh
+++ scripts/RSUC.sh
@@ -2,10 +2,10 @@
 #PBS -q small
 #PBS -l ncpus=1
 #PBS -V
+cd ${PBS_O_WORKDIR}
 
-mkdir -p analysis/RSCU 
-python scripts/list.py "data/dataset3/*.gbff" > analysis/RSCU/list.txt
-cd analysis/RSCU
+mkdir -p analysis/RSCU && cd $_
+python ../../scripts/list.py "../../data/dataset3/*.gbff" > list.txt
 for i in $(cut -f1 list.txt); do Rscript ../../scripts/RSCU.R; done
 
 
