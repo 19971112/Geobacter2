2b684b71a0ada1aa2ab8bce9247f2d96121a6736
19971112 <warashinatomorrow1112@gmail.com>
2019-11-13 17:26:49 +0900

    Update 16S_phylogeny2.sh

--- scripts/16S_phylogeny2.sh
+++ scripts/16S_phylogeny2.sh
@@ -6,11 +6,11 @@ cd ${PBS_O_WORKDIR}
 # extracting 16S rRNA sequence from GenBank files and concatenate
 mkdir -p data/16S_4
 for i in data/dataset3/*.gbff; do python scripts/ex_16SrRNA3.py $i > $i.16S.fasta; mv $i.16S.fasta data/16S_4; done
-cat data/16S_4/*.16S.fasta | sed -e 's/ /_/g' | sed -e 's/,/_/g' | sed -e 's/___/_/g' > data/16S_4/all.fasta
+cat data/16S_4/*.16S.fasta | sed -e 's\/dataset3\/_/g' | sed -e 's/,/_/g' | sed -e 's/___/_/g' > data/16S_4/all.fasta
 
 # Multiple alignment
 mkdir -p analysis/16S_phylogeny_4
-mafft --auto data/16S_4/all.fasta > analysis/16S_phylogeny_3/16S.maf
+mafft --auto data/16S_4/all.fasta > analysis/16S_phylogeny_4/16S.maf
 
 # phylogeny
 FastTree -gtr -nt analysis/16S_phylogeny_4/16S.maf > analysis/16S_phylogeny_4/16S.maf.newick
