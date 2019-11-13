083a49769da0af2f352b3cfcd198b68cc9159e14
19971112 <warashinatomorrow1112@gmail.com>
2019-11-13 22:11:54 +0900

    Update make_replacelist.sh

--- scripts/make_replacelist.sh
+++ scripts/make_replacelist.sh
@@ -4,18 +4,27 @@ mkdir -p data/replacelist && cd $_
 
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt
 
-cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $1,"_",$16,"_genomic","\t",$8}' | cut -d " " -f 1,2 | sed -e 's/ /_/g'> X
-cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $9}' | sed -e 's/strain=//g' | sed -e 's/ /_/g' > Y
-paste -d "_" X Y > replacelist1.txt
+cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $1,"_",$16,"\t"}' | sed -e 's/ /_/g' > 1
+cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $8}' | cut -d " " -f 1,2 | sed -e 's/ /_/g' > 2
+cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print "_", $9}' | sed -e 's/strain=//g' | sed -e 's/ /_/g' > 3
+paste -d "" 1 2 3 > replacelist0.txt
 
-cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $1,"_",$16,"_genomic.gbff","\t",$8}' | cut -d " " -f 1,2 | sed -e 's/ /_/g'> X
-cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $9}' | sed -e 's/strain=//g' | sed -e 's/ /_/g' > Y
-paste -d "_" X Y > replacelist2.txt
+cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $1,"_",$16,"_genomic","\t"}' | sed -e 's/ /_/g' > 1
+cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $8}' | cut -d " " -f 1,2 | sed -e 's/ /_/g' > 2
+cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print "_", $9}' | sed -e 's/strain=//g' | sed -e 's/ /_/g' > 3
+paste -d "" 1 2 3 > replacelist1.txt
 
-cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $1,"_",$16,"_genomic.gbff.fna","\t",$8}' | cut -d " " -f 1,2 | sed -e 's/ /_/g'> X
-cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $9}' | sed -e 's/strain=//g' | sed -e 's/ /_/g' > Y
-paste -d "_" X Y > replacelist3.txt
+cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $1,"_",$16,"_genomic.gbff","\t"}' | sed -e 's/ /_/g' > 1
+cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $8}' | cut -d " " -f 1,2 | sed -e 's/ /_/g' > 2
+cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print "_", $9}' | sed -e 's/strain=//g' | sed -e 's/ /_/g' > 3
+paste -d "" 1 2 3 > replacelist2.txt
 
-cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $1,"_",$16,"_genomic.gbff.faa","\t",$8}' | cut -d " " -f 1,2 | sed -e 's/ /_/g'> X
-cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $9}' | sed -e 's/strain=//g' | sed -e 's/ /_/g' > Y
-paste -d "_" X Y > replacelist4.txt
+cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $1,"_",$16,"_genomic.fna","\t"}' | sed -e 's/ /_/g' > 1
+cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $8}' | cut -d " " -f 1,2 | sed -e 's/ /_/g' > 2
+cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print "_", $9}' | sed -e 's/strain=//g' | sed -e 's/ /_/g' > 3
+paste -d "" 1 2 3 > replacelist3.txt
+
+cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $1,"_",$16,"_genomic.faa","\t"}' | sed -e 's/ /_/g' > 1
+cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print $8}' | cut -d " " -f 1,2 | sed -e 's/ /_/g' > 2
+cat assembly_summary.txt | awk -F "\t" -v 'OFS=' '{print "_", $9}' | sed -e 's/strain=//g' | sed -e 's/ /_/g' > 3
+paste -d "" 1 2 3 > replacelist4.txt
