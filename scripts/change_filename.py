f13d859b571318c6bf5743c67c91d8ab8a13ce92
19971112 <warashinatomorrow1112@gmail.com>
2019-11-16 14:03:52 +0900

    Update change_filename.py

--- scripts/change_filename.py
+++ scripts/change_filename.py
@@ -3,10 +3,19 @@ import glob
 from Bio import SeqIO
 from Bio.SeqUtils import GC
 
+import subprocess
 
 filename = sys.argv[1]
 
 files = glob.glob(filename)
 for gbff in files: 
     for record in SeqIO.parse(gbff, 'genbank'): 
-        print(record.id,gbff,record.description,sep="\t")  
+        print(rec.annotations['organism'])
+#         record.description
+#         print(record.description)
+#         = difinition.strip("\n")
+#         print(record.id,gbff,record.description,sep="\t")  
+
+        
+# cmd = "bash ./change.sh"
+# subprocess.call(cmd.split())
